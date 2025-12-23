d4349b3eb7c56c7a81deb3795e1dca48757b4bee
sreyasstp <165893696+sreyasstp@users.noreply.github.com>
2025-12-23 10:38:59 +0530

    Updated Products.tsx

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -46,7 +46,7 @@ export function Products() {
                 <img
                   src={product.image}
                   alt={product.name}
-                  className="w-full h-full object-contain p-4 group-hover:scale-110 transition-transform duration-500"
+                  className="w-full h-full object-cover group-hover:scale-110 transition-transform duration-500"
                 />
               </div>
 
