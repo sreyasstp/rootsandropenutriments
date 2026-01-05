74329c4e53851ff2c80ad1134dc55d08c581893b
sreyasilp <er.sreyas.tp@gmail.com>
2026-01-05 12:03:58 +0530

    fxi

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // 👈 THIS FIXES /index.html URL ISSUE
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
