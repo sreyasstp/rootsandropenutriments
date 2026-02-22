234d91ff79cec9ef4481db1b3d5104e80c758347
sreyasilp <er.sreyas.tp@gmail.com>
2026-02-22 10:53:44 +0530

    changed api url

--- client/src/services/adminAuth.ts
+++ client/src/services/adminAuth.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API_BASE_URL = import.meta.env.VITE_API_URL || "https://server-rooots.onrender.com";
 
 export const adminLogin = async (email: string, password: string) => {
   const response = await axios.post(`${API_BASE_URL}/api/admin/login`, {
--- client/src/services/authApi.ts
+++ client/src/services/authApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:5000";
+  import.meta.env.VITE_API_URL || "https://server-rooots.onrender.com";
 
 export const googleLoginApi = async (token: string) => {
   const res = await axios.post(`${BASE_URL}/api/auth/google`, {
